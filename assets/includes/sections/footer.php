2930443b60ab0c09fc5fa857c4f834d18782583f
Philip Henning <philiphenning@outlook.com>
2025-08-01 08:40:32 +0100

    Add Footer

--- assets/includes/sections/footer.php
+++ assets/includes/sections/footer.php
@@ -1,3 +1,21 @@
+        <footer>
+
+            <section>
+                
+                <p>
+                    Philip Henning trading as PHD Technology Solutions
+
+                    [
+                    <a href="/pages/contact.php">contact</a>
+                    ||
+                    <a href="/pages/legal.php">legal</a>
+                    ]
+                </p>
+
+            </section>
+
+        </footer>
+    
     </body>
 
 </html>
