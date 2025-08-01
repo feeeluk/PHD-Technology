29ed35cfa09d29b9b07c22074522dcfae3d6c21f
Philip Henning <philiphenning@outlook.com>
2025-08-01 11:42:48 +0100

    Add all page_components to Business Analysis

--- assets/includes/page_components/services/proof_of_concept/proof_of_concept_overview.php
+++ assets/includes/page_components/services/proof_of_concept/proof_of_concept_overview.php
@@ -25,11 +25,6 @@
                 A 'Proof of Concept' (PoC) is a demonstration - often in the form of a small, pilot project or prototype - that tests whether a business idea, product, service, or solution is feasible, practical, and viable before investing more time and resources into full-scale development or deployment.
             </p>
             
-            <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-            
             <?php
 
                 if (str_contains($_SERVER['REQUEST_URI'],'proof_of_concept') == false)
