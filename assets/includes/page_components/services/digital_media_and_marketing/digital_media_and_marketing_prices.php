d79575f26e9731628938cd00c7caf6d375374e4c
Phil Henning <philiphenning@outlook.com>
2026-02-01 20:56:17 +0000

    All - Create 'Pricing' sections

--- assets/includes/page_components/services/digital_media_and_marketing/digital_media_and_marketing_prices.php
+++ assets/includes/page_components/services/digital_media_and_marketing/digital_media_and_marketing_prices.php
@@ -1,9 +1,86 @@
-<?php
+<article class="prices">
+    
+    <h1>PRICES</h1>
 
-    echo "<p>";
+    <section>
 
-        echo "Prices section here..";
+        <div>
 
-    echo "</p>";
+            <span class="container_row">
 
-?>
+                <table>
+
+                    <colgroup class="table_left"></colgroup>
+                    <colgroup span="2" class="table_right"></colgroup>
+
+                    <tb>
+
+                        <tr class="title">
+                            <th colspan="3">RATES</th>
+                        </tr>
+                        
+                        <tr class="sub-title">
+                            <td class="hidden"></td>
+                            <td>Ad-hoc</td>
+                            <td>Block</td>
+                        </tr>
+
+                        <tr>
+                            <td class="item">Initial Consultation</td>
+                            <td>FREE</td>
+                            <td>X</td>
+                        </tr>
+                        
+                        <tr>
+                            <td class="item">1 Hour</td>
+                            <td>£<!--£59--></td>
+                            <td>X</td>
+                        </tr>
+                        
+                        <tr>
+                            <td class="item">1/2 Day</td>
+                            <td>£<!--199--></td>
+                            <td>X</td>
+                        </tr>
+                        
+                        <tr>
+                            <td class="item">1 Day</td>
+                            <td>£<!--£319--></td>
+                            <td>X</td>
+                        </tr>
+                        
+                        <tr>
+                            <td class="item">5 Days</td>
+                            <td>X</td>
+                            <td>£<!--£1,499--></td>
+                        </tr>
+                        
+                        <tr>
+                            <td class="item">10 Days</td>
+                            <td>X</td>
+                            <td>£<!--£2,899--></td>
+                        </tr>
+
+                        <tr>
+                            <td class="item">15 Days</td>
+                            <td>X</td>
+                            <td>£<!--£4,199--></td>
+                        </tr>
+                        
+                        <tr>
+                            <td class="item">20 Days</td>
+                            <td>X</td>
+                            <td>£<!--£4,199--></td>
+                        </tr>
+
+                    </tb>
+                    
+                </table>
+
+            </span>
+            
+        </div>
+
+    </section>
+
+</article>
