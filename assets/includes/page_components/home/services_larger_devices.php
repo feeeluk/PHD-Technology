ab80a84bfc79a26c333ef708f92d23229f934f68
Phil Henning <philiphenning@outlook.com>
2026-02-09 00:39:33 +0000

    Added classes to each of the items within services_large_devices. Started displaying them using flex & flex-grow.

--- assets/includes/page_components/home/services_larger_devices.php
+++ assets/includes/page_components/home/services_larger_devices.php
@@ -1,62 +1,62 @@
 <div id="services_larger_devices">
 
-    <span>
+    <span class="services_tile">
         <a href="#business_analysis">
             <img src="/assets/images/services/business_analysis/link_business_analysis.webp" alt="Business analysis">
             <h3>Business Analysis</h3>
         </a>
     </span>
 
-    <span>
+    <span class="services_tile">
         <a href="#project_management">
             <img src="/assets/images/services/project_management/link_project_management.webp" alt="Project managment">
             <h3>Project Management</h3>
         </a>
     </span>
 
-    <span>
+    <span class="services_tile">
         <a href="#software_development">            
             <img src="/assets/images/services/software_development/link_software_development.webp" alt="Software development">                     
             <h3>Software Development</h3>
         </a>
     </span>
 
-    <span>
+    <span class="services_tile">
         <a href="#data_manipulation">
             <img src="/assets/images/services/data_manipulation/link_data_manipulation.webp" alt="data manipulation">
             <h3>Data Manipulation</h3>
         </a>
     </span>
 
-    <span>
+    <span class="services_tile">
         <a href="#skills_development_and_training">
             <img src="/assets/images/services/skills_development_and_training/link_skills_development_and_training.webp" alt="Skills development & training">
             <h3>Skills Development & Training</h3>
         </a>
     </span>
     
-    <span>
+    <span class="services_tile">
         <a href="#proof_of_concept">
             <img src="/assets/images/services/proof_of_concept/link_proof_of_concept.webp" alt="Proof of concept">
             <h3>Proof of Concept</h3>
         </a>
     </span>
 
-    <span>
+    <span class="services_tile">
         <a href="#artificial_intelligence">
             <img src="/assets/images/services/artificial_intelligence/link_artificial_intelligence.webp" alt="Artificial intelligence">
             <h3>Artificial Intelligence</h3>
         </a>
     </span>
 
-    <span>
+    <span class="services_tile">
         <a href="#digital_media_and_marketing">
             <img src="/assets/images/services/digital_media_and_marketing/link_digital_media_and_marketing.webp" alt="Digital media and marketing"">
             <h3>Digital Media & Marketing</h3>
         </a>
     </span>
     
-    <span>
+    <span class="services_tile">
         <a href="#ui_ux">
             <img src="/assets/images/services/ui_ux/link_ui_ux.webp" alt="UI/UX">
             <h3>UI/UX</h3>
