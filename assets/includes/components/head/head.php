4ebed4af23d0717c02cd5f1bae3d9651bcc55e28
Phil Henning <philiphenning@outlook.com>
2026-02-23 21:15:05 +0000

    Added item--SubHeading class to Software Development > Description

--- assets/includes/components/head/head.php
+++ assets/includes/components/head/head.php
@@ -6,25 +6,25 @@
         <link rel="icon" type="image/x-icon" href="/assets/images/favicon/favicon.ico">
     
         <!-- resets CSS -->
-        <link rel="stylesheet" href="/assets/styles/reset.css?v=12"> 
+        <link rel="stylesheet" href="/assets/styles/reset.css"> 
 
         <!-- sets the look and feel -->
-        <link rel="stylesheet" href="/assets/styles/variables.css?v=12">
+        <link rel="stylesheet" href="/assets/styles/variables.css">
     
         <!-- the core layouts and design -->
-        <link rel="stylesheet" href="/assets/styles/layout_general.css?v=12"> 
-        <link rel="stylesheet" href="/assets/styles/layout_header.css?v=12">
-        <link rel="stylesheet" href="/assets/styles/layout_homepage.css?v=12">
-        <link rel="stylesheet" href="/assets/styles/layout_about.css?v=12">
-        <link rel="stylesheet" href="/assets/styles/layout_services.css?v=12">
-        <link rel="stylesheet" href="/assets/styles/layout_portfolio.css?v=12">
-        <link rel="stylesheet" href="/assets/styles/layout_contact.css?v=12">
+        <link rel="stylesheet" href="/assets/styles/layout_general.css"> 
+        <link rel="stylesheet" href="/assets/styles/layout_header.css">
+        <link rel="stylesheet" href="/assets/styles/layout_homepage.css">
+        <link rel="stylesheet" href="/assets/styles/layout_about.css">
+        <link rel="stylesheet" href="/assets/styles/layout_services.css">
+        <link rel="stylesheet" href="/assets/styles/layout_portfolio.css">
+        <link rel="stylesheet" href="/assets/styles/layout_contact.css">
     
         <!-- components -->
-        <link rel="stylesheet" href="/assets/styles/components.css?v=12">   
+        <link rel="stylesheet" href="/assets/styles/components.css">   
     
         <!-- helper classes -->
-        <link rel="stylesheet" href="/assets/styles/utilities.css?v=12">
+        <link rel="stylesheet" href="/assets/styles/utilities.css">
 
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@2,14..32,12..92;1,14..32,12..92&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@2,12..92;1,12..92&display=swap" rel="stylesheet">
