ddd8d6fb41353ae50b968ced1015da8e607e95fb
Phil Henning <philiphenning@outlook.com>
2026-02-23 17:40:44 +0000

    updated css version #

--- assets/includes/components/head/head.php
+++ assets/includes/components/head/head.php
@@ -1,33 +1,33 @@
 <head>
 
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.2">
 
         <link rel="icon" type="image/x-icon" href="/assets/images/favicon/favicon.ico">
     
         <!-- resets CSS -->
-        <link rel="stylesheet" href="/assets/styles/reset.css?v=11"> 
+        <link rel="stylesheet" href="/assets/styles/reset.css?v=12"> 
 
         <!-- sets the look and feel -->
-        <link rel="stylesheet" href="/assets/styles/variables.css?v=11">
+        <link rel="stylesheet" href="/assets/styles/variables.css?v=12">
     
         <!-- the core layouts and design -->
-        <link rel="stylesheet" href="/assets/styles/layout_general.css?v=11"> 
-        <link rel="stylesheet" href="/assets/styles/layout_header.css?v=11">
-        <link rel="stylesheet" href="/assets/styles/layout_homepage.css?v=11">
-        <link rel="stylesheet" href="/assets/styles/layout_about.css?v=11">
-        <link rel="stylesheet" href="/assets/styles/layout_services.css?v=11">
-        <link rel="stylesheet" href="/assets/styles/layout_portfolio.css?v=11">
-        <link rel="stylesheet" href="/assets/styles/layout_contact.css?v=11">
+        <link rel="stylesheet" href="/assets/styles/layout_general.css?v=12"> 
+        <link rel="stylesheet" href="/assets/styles/layout_header.css?v=12">
+        <link rel="stylesheet" href="/assets/styles/layout_homepage.css?v=12">
+        <link rel="stylesheet" href="/assets/styles/layout_about.css?v=12">
+        <link rel="stylesheet" href="/assets/styles/layout_services.css?v=12">
+        <link rel="stylesheet" href="/assets/styles/layout_portfolio.css?v=12">
+        <link rel="stylesheet" href="/assets/styles/layout_contact.css?v=12">
     
         <!-- components -->
-        <link rel="stylesheet" href="/assets/styles/components.css?v=11">   
+        <link rel="stylesheet" href="/assets/styles/components.css?v=12">   
     
         <!-- helper classes -->
-        <link rel="stylesheet" href="/assets/styles/utilities.css?v=11">
+        <link rel="stylesheet" href="/assets/styles/utilities.css?v=12">
 
-    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@1,14..32,11..91;1,14..32,11..91&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@1,11..91;1,11..91&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@2,14..32,12..92;1,14..32,12..92&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@2,12..92;1,12..92&display=swap" rel="stylesheet">
     
     <script defer src="/assets/scripts/script.js"></script>
 
