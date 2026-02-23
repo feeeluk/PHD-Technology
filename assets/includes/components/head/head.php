1c7fea7467f2c2b5d0ad670ae5d1b72d77fe5b57
Phil Henning <philiphenning@outlook.com>
2026-02-23 17:33:34 +0000

    Changed hamburger icon to be smaller and same colour as logo

--- assets/includes/components/head/head.php
+++ assets/includes/components/head/head.php
@@ -1,33 +1,33 @@
 <head>
 
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.1">
 
         <link rel="icon" type="image/x-icon" href="/assets/images/favicon/favicon.ico">
     
         <!-- resets CSS -->
-        <link rel="stylesheet" href="/assets/styles/reset.css?v=10"> 
+        <link rel="stylesheet" href="/assets/styles/reset.css?v=11"> 
 
         <!-- sets the look and feel -->
-        <link rel="stylesheet" href="/assets/styles/variables.css?v=10">
+        <link rel="stylesheet" href="/assets/styles/variables.css?v=11">
     
         <!-- the core layouts and design -->
-        <link rel="stylesheet" href="/assets/styles/layout_general.css?v=10"> 
-        <link rel="stylesheet" href="/assets/styles/layout_header.css?v=10">
-        <link rel="stylesheet" href="/assets/styles/layout_homepage.css?v=10">
-        <link rel="stylesheet" href="/assets/styles/layout_about.css?v=10">
-        <link rel="stylesheet" href="/assets/styles/layout_services.css?v=10">
-        <link rel="stylesheet" href="/assets/styles/layout_portfolio.css?v=10">
-        <link rel="stylesheet" href="/assets/styles/layout_contact.css?v=10">
+        <link rel="stylesheet" href="/assets/styles/layout_general.css?v=11"> 
+        <link rel="stylesheet" href="/assets/styles/layout_header.css?v=11">
+        <link rel="stylesheet" href="/assets/styles/layout_homepage.css?v=11">
+        <link rel="stylesheet" href="/assets/styles/layout_about.css?v=11">
+        <link rel="stylesheet" href="/assets/styles/layout_services.css?v=11">
+        <link rel="stylesheet" href="/assets/styles/layout_portfolio.css?v=11">
+        <link rel="stylesheet" href="/assets/styles/layout_contact.css?v=11">
     
         <!-- components -->
-        <link rel="stylesheet" href="/assets/styles/components.css?v=10">   
+        <link rel="stylesheet" href="/assets/styles/components.css?v=11">   
     
         <!-- helper classes -->
-        <link rel="stylesheet" href="/assets/styles/utilities.css?v=10">
+        <link rel="stylesheet" href="/assets/styles/utilities.css?v=11">
 
-    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@1,14..32,11..91;1,14..32,11..91&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@1,11..91;1,11..91&display=swap" rel="stylesheet">
     
     <script defer src="/assets/scripts/script.js"></script>
 
