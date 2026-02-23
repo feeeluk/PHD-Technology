6cddb5a38f20ae2fcd9791a0837fb8a145b9b40d
Phil Henning <philiphenning@outlook.com>
2026-02-23 21:43:27 +0000

    Revert "Actually fixed bug - Anchor points on smaller devices do not work as expected"
    This reverts commit 55e06b9c45016127b8b71bbd331cddb9fe0180e1.

--- assets/includes/components/head/head.php
+++ assets/includes/components/head/head.php
@@ -6,25 +6,25 @@
         <link rel="icon" type="image/x-icon" href="/assets/images/favicon/favicon.ico">
     
         <!-- resets CSS -->
-        <link rel="stylesheet" href="/assets/styles/reset.css"> 
+        <link rel="stylesheet" href="/assets/styles/reset.css?v=9"> 
 
         <!-- sets the look and feel -->
-        <link rel="stylesheet" href="/assets/styles/variables.css">
+        <link rel="stylesheet" href="/assets/styles/variables.css?v=9">
     
         <!-- the core layouts and design -->
-        <link rel="stylesheet" href="/assets/styles/layout_general.css"> 
-        <link rel="stylesheet" href="/assets/styles/layout_header.css">
-        <link rel="stylesheet" href="/assets/styles/layout_homepage.css">
-        <link rel="stylesheet" href="/assets/styles/layout_about.css">
-        <link rel="stylesheet" href="/assets/styles/layout_services.css">
-        <link rel="stylesheet" href="/assets/styles/layout_portfolio.css">
-        <link rel="stylesheet" href="/assets/styles/layout_contact.css">
+        <link rel="stylesheet" href="/assets/styles/layout_general.css?v=9"> 
+        <link rel="stylesheet" href="/assets/styles/layout_header.css?v=9">
+        <link rel="stylesheet" href="/assets/styles/layout_homepage.css?v=9">
+        <link rel="stylesheet" href="/assets/styles/layout_about.css?v=9">
+        <link rel="stylesheet" href="/assets/styles/layout_services.css?v=9">
+        <link rel="stylesheet" href="/assets/styles/layout_portfolio.css?v=9">
+        <link rel="stylesheet" href="/assets/styles/layout_contact.css?v=9">
     
         <!-- components -->
-        <link rel="stylesheet" href="/assets/styles/components.css">   
+        <link rel="stylesheet" href="/assets/styles/components.css?v=9">   
     
         <!-- helper classes -->
-        <link rel="stylesheet" href="/assets/styles/utilities.css">
+        <link rel="stylesheet" href="/assets/styles/utilities.css?v=9">
 
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@2,14..32,12..92;1,14..32,12..92&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@2,12..92;1,12..92&display=swap" rel="stylesheet">
