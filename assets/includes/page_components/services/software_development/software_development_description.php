03ec68c8347109840b6b8cce7c7bb2badbd6b22a
Phil Henning <philiphenning@outlook.com>
2026-02-24 08:49:45 +0000

    Adjusted Software Development sub section headings to Mobile First design

--- assets/includes/page_components/services/software_development/software_development_description.php
+++ assets/includes/page_components/services/software_development/software_development_description.php
@@ -19,7 +19,7 @@
 
         <img src="/assets/images/services/software_development/overview_dot_net_development.webp" alt=".NET devlopment">
 
-        <h3>.NET Development</h3>
+        <h2 class="item--SubSectionHeading">.NET</h2>
 
         <p>
             .NET development is the process of building software applications using Microsoft’s .NET framework. It’s a popular platform for creating a wide range of apps—from web and mobile to desktop, cloud, gaming, and IoT solutions.
@@ -36,7 +36,7 @@
 
         <div id="DotNetDevelopmentDetailsOfService" class="detailsOfService">
 
-            <h3>.Net development with PHD Technology</h3>
+            <h3>.NET development with PHD Technology</h3>
 
             <p>
                 TEST. This paragraph should only be visible in the specific service page, not on the homepage.
@@ -55,7 +55,7 @@
 
         <img src="/assets/images/services/software_development/overview_web_development.webp" alt="Web development">
         
-        <h3>Web Development</h3>
+        <h2 class="item--SubSectionHeading">Web</h2>
         
         <p>
             Web development is the process of creating and maintaining websites or web applications that run on the internet (or an intranet). It involves everything from designing how a site looks to making sure it functions smoothly behind the scenes.
