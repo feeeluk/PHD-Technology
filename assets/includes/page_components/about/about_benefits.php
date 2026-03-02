41d2b96ac29b98317b8e01a9f84fe0f2abf79948
Phil Henning <philiphenning@outlook.com>
2026-03-02 10:58:27 +0000

    Bug fix - text mistakes in About > Description - tweaks

--- assets/includes/page_components/about/about_benefits.php
+++ assets/includes/page_components/about/about_benefits.php
@@ -7,7 +7,7 @@
         <h2 class="benefits--Heading">Low cost</h2>
         
         <p class="benefits--Text">
-            Not only are our services very reasonably priced to begin with, they are exceptionally lower than the equivalent costs of taking on one or more contractors or salaried staff.
+            Not only are our services very reasonably priced, they are exceptionally lower than the equivalent costs of taking on contractors or salaried staff.
         </p>
 
     </div>
@@ -17,7 +17,7 @@
         <h2 class="benefits--Heading">High quality</h2>
 
         <p class="benefits--Text">
-            With years of experience working with many major multi-national businesses, using modern technologies and the latest research, the quality of our services is second to none.
+            With years of experience working with many major multi-national businesses, the quality of our services is second to none.
         </p>
 
     </div>
@@ -27,7 +27,7 @@
         <h2 class="benefits--Heading">Low risk</h2>
 
         <p class="benefits--Text">
-            Unlike hiring a contractor or salaried staff, our services do not come with any lengthy commitments or financial penalty clauses. Also, as our services do not intefere with the day-to-day operation of your business, there are no nagging or lingering ties. 
+            Unlike hiring a contractor or salaried staff, our services do not come with any lengthy commitments or financial penalty clauses. 
         </p>
 
     </div>
@@ -57,7 +57,9 @@
         <h2 class="benefits--Heading">Flexible</h2>
 
         <p class="benefits--Text">
-            Use us when you need us. We can help with many different aspects of your business.
+            We can help you with many different aspects of your business.
+            <br>
+            Use us as and when you need us. Not when you don't.
         </p>
 
     </div>
@@ -67,7 +69,7 @@
         <h2 class="benefits--Heading">Adds value to your business</h2>
 
         <p class="benefits--Text">
-            Without any of the overheads or commitments.
+            Add value without incurring any of the overheads or commitments.
         </p>
 
     </div>
