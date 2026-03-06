e9bcdcbb9440978e66aa895de3d5fe7083b24671
Phil Henning <philiphenning@outlook.com>
2026-03-06 18:52:28 +0000

    Various changes to the navigation disclosure. Made the code cleaner, easier to read, annotated it, and changed some of the workings and styling

--- assets/includes/components/header/nav_smaller_devices.php
+++ assets/includes/components/header/nav_smaller_devices.php
@@ -1,3 +1,5 @@
+<!-- Disclosure One -->
+<!-- ********************** -->
 <details id="DisclosureOne">
 
     <summary>
@@ -8,32 +10,109 @@
     
     <nav id="NavbarForMobile">
 
-        <a href="/"><span class="smallDeviceMenuLink">Home</span></a>
-        <a href="/pages/about.php"><span class="smallDeviceMenuLink">About</span></a>
+        <a href="/">
+            <span class="smallDeviceMenuLink levelOne">
+                Home
+            </span>
+        </a>
 
+        <a href="/pages/about.php">
+            <span class="smallDeviceMenuLink levelOne">
+                About
+            </span>
+        </a>
+
+        <!-- Disclosure Two -->
+        <!-- ********************** -->
+        
         <details id="DisclosureTwo">
             
-            <summary>
+            <summary class="smallDeviceMenuLink levelOne">
+
+                <span>
+                    Services
+                </span>
                 
-                Services <span class="arrow"></span>
-                
+                <span class="arrow">
+                </span> 
+
             </summary>
             
-            <a href="/pages/services/business_analysis.php"><span class="smallDeviceMenuLink indentLink">Business Analysis</span></a>
-            <a href="/pages/services/data.php"><span class="smallDeviceMenuLink indentLink">Data</span></a>
-            <a href="/pages/services/proof_of_concept.php"><span class="smallDeviceMenuLink indentLink">Proof of Concept</span></a>
-            <a href="/pages/services/software_development.php"><span class="smallDeviceMenuLink indentLink">Software Development</span></a>
-            <a href="/pages/services/digital_transformation.php"><span class="smallDeviceMenuLink indentLink">Digital Transformation</span></a>
-            <a href="/pages/services/ui_ux.php"><span class="smallDeviceMenuLink indentLink">UI/UX</span></a>
-            <a href="/pages/services/skills_development_and_training.php"><span class="smallDeviceMenuLink indentLink">Skills Development & Training</span></a>
-            <a href="/pages/services/artificial_intelligence.php"><span class="smallDeviceMenuLink indentLink">Artificial Intelligence</span></a>
-            <a href="/pages/services/digital_media_and_marketing.php"><span class="smallDeviceMenuLink indentLink">Digital Media & Marketing</span></a>
+            <a href="/pages/services/business_analysis.php">
+                <span class="smallDeviceMenuLink indentLink levelTwo">
+                    Business Analysis
+                </span>
+            </a>
+            
+            <a href="/pages/services/data.php">
+                <span class="smallDeviceMenuLink indentLink levelTwo">
+                    Data
+                </span>
+            </a>
+
+            <a href="/pages/services/proof_of_concept.php">
+                <span class="smallDeviceMenuLink indentLink levelTwo">
+                    Proof of Concept
+                </span>
+            </a>
+
+            <a href="/pages/services/software_development.php">
+                <span class="smallDeviceMenuLink indentLink levelTwo">
+                    Software Development
+                </span>
+            </a>
+
+            <a href="/pages/services/digital_transformation.php">
+                <span class="smallDeviceMenuLink indentLink levelTwo">
+                    Digital Transformation
+                </span>
+            </a>
+
+            <a href="/pages/services/ui_ux.php">
+                <span class="smallDeviceMenuLink indentLink levelTwo">
+                    UI/UX
+                </span>
+            </a>
+
+            <a href="/pages/services/skills_development_and_training.php">
+                <span class="smallDeviceMenuLink indentLink levelTwo">
+                    Skills Development & Training
+                </span>
+            </a>
+
+            <a href="/pages/services/artificial_intelligence.php">
+                <span class="smallDeviceMenuLink indentLink levelTwo">
+                    Artificial Intelligence
+                </span>
+            </a>
+
+            <a href="/pages/services/digital_media_and_marketing.php">
+                <span class="smallDeviceMenuLink indentLink levelTwo">
+                    Digital Media & Marketing
+                </span>
+            </a>
             
         </details>
 
-        <!-- <a href="/pages/case_studies.php"><span class="smallDeviceMenuLink">Case Studies</span></a> -->
-        <a href="/pages/portfolio.php"><span class="smallDeviceMenuLink">Portfolio</span></a>
-        <a href="/pages/contact.php"><span class="smallDeviceMenuLink">Contact</span></a>
+        <!--
+        <a href="/pages/case_studies.php">
+            <span class="smallDeviceMenuLink">
+                Case Studies
+            </span>
+        </a>
+        -->
+        
+        <a href="/pages/portfolio.php">
+            <span class="smallDeviceMenuLink levelOne">
+                Portfolio
+            </span>
+        </a>
+        
+        <a href="/pages/contact.php">
+            <span class="smallDeviceMenuLink levelOne">
+                Contact
+            </span>
+        </a>
         
     </nav>
     
