4fb39edc607fb7d1ffa87c6c095d6fd783ea8731
Phil Henning <philiphenning@outlook.com>
2026-03-29 13:09:17 +0100

    Edited footer to make logos centered

--- assets/includes/layout/footer.php
+++ assets/includes/layout/footer.php
@@ -1,41 +1,35 @@
-<div class="footer--Left">
-    <p>
-        |
-        <a href="/pages/contact.php" aria-label="Contact page">contact</a>
-        |
-        <a href="/pages/legal.php" aria-label="Legal page">legal</a>
-        |
-        <a href="/assets/public_files/files.php" aria-label="Public files folder">files</a>
-        |
-        
-    </p>
+<p class="footer--Left">
 
-    <p>
-        Philip Henning trading as PDH Technology
-    </p>
-
-</div>
-
-<div class="footer--Right">
-    <span class="footer--Logo-Container">
-
-        <a href="https://www.linkedin.com/company/pdh-technology" aria-label="Linkedin page" target="_blank">
-            <img src="/assets/images/footer/linkedin.png" alt="LinkedIn page" class="footer--Logo">
-        </a>
-
-        <a href="https://www.facebook.com/profile.php?id=61577531184344" aria-label="Facebook page" target="_blank">
-            <img src="/assets/images/footer/facebook.png" alt="Facebook page" class="footer--Logo">
-        </a>
-        
-        <a href="https://trello.com/invite/b/692dce0d19aab9be3d84fa4a/ATTIa7da0c88000220adb6717ed94cb880da492D4337/pdh-technology" aria-label="Trello page" target="_blank">
-            <img src="/assets/images/footer/trello.jpg" alt="Trello page" class="footer--Logo">
-        </a>
-        
-        <a href="https://github.com/feeeluk" aria-label="Github page" target="_blank">
-            <img src="/assets/images/footer/github.webp" alt="Github page" class="footer--Logo">
-        </a> 
-
-    </span>
+    |
+    <a href="/pages/contact.php" aria-label="Contact page">contact</a>
+    |
+    <a href="/pages/legal.php" aria-label="Legal page">legal</a>
+    |
+    <a href="/assets/public_files/files.php" aria-label="Public files folder">files</a>
+    |
     
-</div>
+</p>
 
+<p class="footer--Right">
+    
+    <a href="https://www.linkedin.com/company/pdh-technology" aria-label="Linkedin page" target="_blank">
+        <img src="/assets/images/footer/linkedin.png" alt="LinkedIn page" class="footer--Logo">
+    </a>
+
+    <a href="https://www.facebook.com/profile.php?id=61577531184344" aria-label="Facebook page" target="_blank">
+        <img src="/assets/images/footer/facebook.png" alt="Facebook page" class="footer--Logo">
+    </a>
+    
+    <a href="https://trello.com/invite/b/692dce0d19aab9be3d84fa4a/ATTIa7da0c88000220adb6717ed94cb880da492D4337/pdh-technology" aria-label="Trello page" target="_blank">
+        <img src="/assets/images/footer/trello.jpg" alt="Trello page" class="footer--Logo">
+    </a>
+    
+    <a href="https://github.com/feeeluk" aria-label="Github page" target="_blank">
+        <img src="/assets/images/footer/github.webp" alt="Github page" class="footer--Logo">
+    </a> 
+
+</p>
+
+<p>
+    Philip Henning trading as PDH Technology
+</p>
