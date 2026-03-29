e2266a2326eb56fd64a658e77fd31081cf373601
Phil Henning <philiphenning@outlook.com>
2026-03-29 11:48:57 +0100

    Added OpenGraph info

--- assets/includes/layout/head.php
+++ assets/includes/layout/head.php
@@ -1,48 +1,55 @@
 <head>
 
-    <!-- Metadata -->
-    <meta charset="UTF-8">
+      <!-- Metadata -->
+      <meta charset="UTF-8">
 
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
+      <meta name="viewport"
+      content="width=device-width, initial-scale=1.0">
 
-    <meta name="author"
-          content="Philip Henning">
+      <meta name="author"
+      content="Philip Henning">
 
-    <meta name="description"
-          content="PDH Technology: We help other businesses make better use of their data, tools, ideas, and technology. 
-          <?php echo $metaDescription; ?>">
-        
-    <meta name="keywords"
-          content="PDH Technology, Technology, Ideas, Help, Consulting, Advice, Business Services, 
-          <?php echo $metaKeywords; ?>">
+      <meta name="description"
+      content="PDH Technology: We help other businesses make better use of their data, tools, ideas, and technology. 
+      <?php echo $metaDescription; ?>">
 
-    <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="/assets/images/favicon/favicon.ico">
+      <meta name="keywords"
+      content="PDH Technology, Technology, Ideas, Help, Consulting, Advice, Business Services, 
+      <?php echo $metaKeywords; ?>">
 
-    <!-- CSS -->
-    <link rel="stylesheet" href="/assets/styles/reset.css"> 
-    <link rel="stylesheet" href="/assets/styles/variables.css">
-    <link rel="stylesheet" href="/assets/styles/layout_general.css"> 
-    <link rel="stylesheet" href="/assets/styles/layout_header.css">
-    <link rel="stylesheet" href="/assets/styles/layout_homepage.css">
-    <link rel="stylesheet" href="/assets/styles/layout_about.css">
-    <link rel="stylesheet" href="/assets/styles/layout_services.css">
-    <link rel="stylesheet" href="/assets/styles/layout_portfolio.css">
-    <link rel="stylesheet" href="/assets/styles/layout_contact.css">
-    <link rel="stylesheet" href="/assets/styles/components.css">   
-    <link rel="stylesheet" href="/assets/styles/utilities.css">
+      <!-- Open Graph -->
+      <meta property="og:title" content="PDH Technology">
+      <meta property="og:description" content="We help other businesses make better use of their data, tools, ideas, and technology.">
+      <meta property="og:image" content="https://pdhtechnology.com/assets/images/open_graph/open_graph.webp">
+      <meta property="og:url" content="https://pdhtechnology.com">
+      <meta property="og:type" content="website">
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    
-    <!-- Javascript -->
-    <script defer src="/assets/scripts/javascript/script.js">
-    </script>
+      <!-- Favicon -->
+      <link rel="icon" type="image/x-icon" href="/assets/images/favicon/favicon.ico">
 
-    <title>
-        <?= $subTitle; ?> | <?= $siteTitle; ?>
-    </title>
+      <!-- CSS -->
+      <link rel="stylesheet" href="/assets/styles/reset.css"> 
+      <link rel="stylesheet" href="/assets/styles/variables.css">
+      <link rel="stylesheet" href="/assets/styles/layout_general.css"> 
+      <link rel="stylesheet" href="/assets/styles/layout_header.css">
+      <link rel="stylesheet" href="/assets/styles/layout_homepage.css">
+      <link rel="stylesheet" href="/assets/styles/layout_about.css">
+      <link rel="stylesheet" href="/assets/styles/layout_services.css">
+      <link rel="stylesheet" href="/assets/styles/layout_portfolio.css">
+      <link rel="stylesheet" href="/assets/styles/layout_contact.css">
+      <link rel="stylesheet" href="/assets/styles/components.css">   
+      <link rel="stylesheet" href="/assets/styles/utilities.css">
+
+      <!-- Fonts -->
+      <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+      <!-- Javascript -->
+      <script defer src="/assets/scripts/javascript/script.js">
+      </script>
+
+      <title>
+      <?= $subTitle; ?> | <?= $siteTitle; ?>
+      </title>
     
 </head>
