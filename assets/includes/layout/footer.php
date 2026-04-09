cb2d71bf00c5f67d8e1a92ebe3bf4b7ec78dfcc5
Phil Henning <philiphenning@outlook.com>
2026-04-09 17:26:17 +0100

    removed link to Trello from the footer, created the Release Notes process and page, and then added a link in the Footer to the Release Notes page

--- assets/includes/layout/footer.php
+++ assets/includes/layout/footer.php
@@ -8,6 +8,8 @@
         |
         <a href="/assets/public_files/files.php" aria-label="Public files folder">files</a>
         |
+        <a href="/pages/release_notes.php" aria-label="Release Notes page">Release Notes</a>
+        |
         
     </p>
 
@@ -27,9 +29,9 @@
             <img src="/assets/images/footer/facebook.png" alt="Facebook page" class="footer--Logo">
         </a>
         
-        <a href="https://trello.com/invite/b/692dce0d19aab9be3d84fa4a/ATTIa7da0c88000220adb6717ed94cb880da492D4337/pdh-technology" aria-label="Trello page" target="_blank">
+        <!-- <a href="https://trello.com/invite/b/692dce0d19aab9be3d84fa4a/ATTIa7da0c88000220adb6717ed94cb880da492D4337/pdh-technology" aria-label="Trello page" target="_blank">
             <img src="/assets/images/footer/trello.jpg" alt="Trello page" class="footer--Logo">
-        </a>
+        </a> -->
         
         <a href="https://github.com/feeeluk" aria-label="Github page" target="_blank">
             <img src="/assets/images/footer/github.webp" alt="Github page" class="footer--Logo">
---
+++ pages/release_notes.php
@@ -0,0 +1,33 @@
+<?php
+    
+    //CONFIG
+    include($_SERVER['DOCUMENT_ROOT'] . "/assets/config/config.php");      
+        
+    // PAGE META
+    $subTitle = "Release Notes";
+    $metaDescription = "Relase Notes";
+    $metaKeywords ="Release Notes Information";
+
+?>
+
+<?php include($root."/assets/includes/layout/page_open.php"); ?>
+        
+    <div class="box--WithPadding item text">
+
+        <h1 class="item--Title">
+            Release Notes
+        </h1>
+
+        <ul>
+            <li>
+                <a href="/assets/release_notes/historical/Red Barron.pdf" target="_blank">
+                    Red Barron (30th March 2026 – 5th April 2026)
+                </a>
+            </li>
+        </ul>
+            
+        </p>
+
+    </div>
+
+<?php include($root."/assets/includes/layout/page_close.php"); ?>
