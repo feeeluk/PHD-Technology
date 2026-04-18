3ea5484fae034679b8024c019c4d162de2982571
Philip Henning <philiphenning@outlook.com>
2026-04-18 20:51:36 +0100

    added a 404 file and a .htaccess file for Ionos

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+
+    //CONFIG
+    include($_SERVER['DOCUMENT_ROOT'] . "/assets/config/config.php");
+
+    // PAGE META
+    $subTitle = "404";
+    $metaDescription = "404";
+    $metaKeywords ="Business Analysis, Product Owner, Software Development, Web Development, .Net Development, Project Management, Digital Migrations, Data Maniupulation, Data Management, Artificial Intelligence, Skills Development, Training, Digital Media & Marketing, UI, UX, UI/UX, Proof Of Concept, Hull, Willerby, East Yorkshire, Nationwide";
+
+?>
+
+<?php include($root ."/assets/includes/layout/page_open.php"); ?>
+
+<h1>404 – Page Not Found</h1>
+
+<?php include($root."/assets/includes/layout/page_close.php"); ?>
